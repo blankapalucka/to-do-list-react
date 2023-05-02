d546afabaa483446f3cf2d7c0270f1fe1ba70f45
Blanka Pałucka <blankapalucka@gmail.com>
2023-05-02 18:37:20 +0200

    adding key to li

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -4,8 +4,8 @@ import "./style.css";
 const Tasks = ({ tasks, hideDoneTasks }) => (
     <ul className="tasks">
         {tasks.map(task => (
-            <li
-                className={`tasks__item${task.done && hideDoneTasks ? " tasks__item--hidden" : ""}`}>
+            <li key={task.id}           
+                 className={`tasks__item${task.done && hideDoneTasks ? " tasks__item--hidden" : ""}`}>
                 <button className="tasks__buton tasks__button--toggleDone">
                     {task.done ? "✔" : ""}
                 </button>
