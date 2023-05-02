28e5e51f1efac74d3cb588c01c618431894e0e3e
Blanka Pałucka <blankapalucka@gmail.com>
2023-05-02 13:17:20 +0200

    taks list without interactions

---
+++ src/Tasks/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./style.css";
+
+const Tasks = ({tasks, hideDoneTasks}) => (
+    <ul className="tasks">
+        {tasks.map(task => (
+            <li
+                className={`tasks__item${task.done && hideDoneTasks ? " tasks__item--hidden" : ""}`}
+            >
+
+                <button className="tasks__buton tasks__button--toggleDone">        
+                {task.done ? "✔" : ""}
+            </button>
+            <span className={`task.done ? " tasks__content--done" : ""`}>
+                {task.content}
+            </span>
+            <button className="tasks__button--remove">🗑️</button>
+        
+            </li>
+        ))}
+    </ul>
+
+);
+
+export default Tasks;
