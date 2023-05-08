38b6afe26e5c2a3f0c6b6a90e5fcfd9e121bdd25
Blanka Pałucka <blankapalucka@gmail.com>
2023-05-08 17:49:55 +0200

    toggle task done interaction

--- src/App.js
+++ src/App.js
@@ -20,9 +20,18 @@ function App() {
   };
 
   const removeTask = (id) => {
-    setTasks(tasks => tasks.filter (task => task.id !== id));
+    setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
+
+      return task;
+    }));
+  }
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -32,7 +41,13 @@ function App() {
       />
       <Section
         title="Lista zadań"
-        body={<Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} />
+        body={
+        <Tasks
+          tasks={tasks} h
+          ideDone={hideDone}
+          removeTask={removeTask}
+          toggleTaskDone={toggleTaskDone}
+        />
         }
         extraHeaderContent={
           <Buttons
